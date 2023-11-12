14821ea099b0b4d9d8d4e13f7e76930dd7219173
Thomas Lohse <thomas.k.lohse@gmail.com>
2023-11-12 16:54:01 +0100

    fmt

--- healthcheck.js
+++ healthcheck.js
@@ -1,26 +1,26 @@
 // https://anthonymineo.com/docker-healthcheck-for-your-node-js-app/
-const http = require('http');
+const http = require("http");
 const options = {
-  host: '0.0.0.0',
-  path: process.env.STATUS_PATH,
-  port: process.env.PORT,
-  timeout: 2000,
+    host: "0.0.0.0",
+    path: process.env.STATUS_PATH,
+    port: process.env.PORT,
+    timeout: 2000,
 };
 
 const healthCheck = http.request(options, (res) => {
-  console.log(`HEALTHCHECK STATUS: ${res.statusCode}`);
-  // 200: We are ready to receieve requests.
-  // 202: We are currently starting up.
-  if (res.statusCode == 200 || res.statusCode == 202) {
-    process.exit(0);
-  } else if (res.statusCode == 500) {
-    process.exit(1);
-  }
+    console.log(`HEALTHCHECK STATUS: ${res.statusCode}`);
+    // 200: We are ready to receieve requests.
+    // 202: We are currently starting up.
+    if (res.statusCode == 200 || res.statusCode == 202) {
+        process.exit(0);
+    } else if (res.statusCode == 500) {
+        process.exit(1);
+    }
 });
 
-healthCheck.on('error', function (err) {
-  console.error('ERROR');
-  process.exit(1);
+healthCheck.on("error", function (err) {
+    console.error("ERROR");
+    process.exit(1);
 });
 
 healthCheck.end();
--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World!"))
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(3000, () => {
     console.log("Server started on port 3000!");

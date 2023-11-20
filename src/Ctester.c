1a4abc886c83a246766236910991f74c78a34c91
lugialukas <lugialukas@live.dk>
2023-11-20 09:43:46 +0100

    set up c files and search for json files

--- src/Ctester.c
+++ src/Ctester.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "filepath/opgaveFile.c" indsæt rigtig filepath hvor de bliver genereret henne
-//#include "filepath/testFile.c"
+#include "test_runner/src/opgFile.c"
+#include "test_runner/src/testFIle.c"
 
 int main(){
     run_all_tests(); //navngives altid sådan her af underviseren
--- src/converter.ts
+++ src/converter.ts
@@ -1,33 +1,55 @@
-import { appendFile } from 'node:fs';
+import { appendFile } from "node:fs";
+import * as fs from "fs";
 
+//import * as opgFile from "./opgFile.json";
+//import * as testFile from "./testFile.json";
+
+//let opg: opgave = JSON.parse(opgFile);
+//let test: tests = JSON.parse(testFile);
 
 interface opgave {
     kode: string;
     sprog: string;
-	exerciseID: string;
+    exerciseID: string;
 }
 
 interface tests {
-	kode: string;
+    kode: string;
 }
 
-//let opg: opgave = JSON.parse(opgFile.json); indsæt de rigtige navne
-//let test: tests = JSON.parse(testFile.json);
+let opg: opgave;
+let test: tests;
 
-appendFile(`opgaveFile.${opg.sprog}`, `${opg.kode}`, 'utf8', function (err) {
-  if (err) {
-    console.log(err)
-    // append failed
-  } else {
-    console.log(`opgaveFile.${opg.sprog} was appended!`)
-    // done
-  }
+try {
+    const opgFile = fs.readFileSync("./opgFile.json", "utf8");
+    opg = JSON.parse(opgFile);
+} catch (err) {
+    console.log("Error reading opgFile.json:", err);
+    throw err;
+}
+
+try {
+    const testFile = fs.readFileSync("./testFile.json", "utf8");
+    test = JSON.parse(testFile);
+} catch (err) {
+    console.log("Error reading testFile.json:", err);
+    throw err;
+}
+
+appendFile(`opgaveFile.${opg.sprog}`, `${opg.kode}`, "utf8", function (err) {
+    if (err) {
+        console.log(err);
+        // append failed
+    } else {
+        console.log(`opgaveFile.${opg.sprog} was appended!`);
+        // done
+    }
 });
 
-appendFile(`testFile.${opg.sprog}`, `${test.kode}`, 'utf8', function (err) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(`testFile.${opg.sprog} was appended!`)
-  }
+appendFile(`testFile.${opg.sprog}`, `${test.kode}`, "utf8", function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`testFile.${opg.sprog} was appended!`);
+    }
 });

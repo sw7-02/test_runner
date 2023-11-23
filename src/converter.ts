76bed92a29858e4a4f1454cb0438742c99774191
lugialukas <lugialukas@live.dk>
2023-11-23 12:29:36 +0100

    Create files and dir

--- src/converter.ts
+++ src/converter.ts
@@ -1,47 +1,61 @@
 import { appendFile } from "node:fs";
 import * as fs from "fs";
 
-interface tasks {
-    code: string;
+interface exerciseAPI {
     lang: string;
+    code: string;
     exerciseID: string;
+    studentID: string;
 }
 
 interface tests {
     code: string;
 }
 
-let task: tasks;
-let test: tests;
 
-try {
-    const taskFile = fs.readFileSync("./taskFile.json", "utf8");
-    task = JSON.parse(taskFile);
-} catch (err) {
-    console.log("Error reading taskFile.json:", err);
-    throw err;
+function createDirectory(exerciseAPI: exerciseAPI, tests: tests): void {
+    if (!fs.existsSync(exerciseAPI.studentID)) {
+        fs.mkdirSync(exerciseAPI.studentID);
+        console.log(`Directory ${exerciseAPI.studentID} created!`);
+    }
+    else {
+        console.log(`Directory ${exerciseAPI.studentID} already exists!`);
+    }
+
+    createFiles(exerciseAPI, tests);
+
 }
 
-try {
-    const testFile = fs.readFileSync("./testFile.json", "utf8");
-    test = JSON.parse(testFile);
-} catch (err) {
-    console.log("Error reading testFile.json:", err);
-    throw err;
+const createFiles = (exerciseAPI: exerciseAPI, tests: tests): void => {
+    appendFile(`${exerciseAPI.studentID}/exerciseFile.${exerciseAPI.lang}`, `${exerciseAPI.code}`, "utf8", function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`exerciseFile.${exerciseAPI.lang} was appended!`);
+        }
+    });
+
+    appendFile(`${exerciseAPI.studentID}/tests/testFile.${exerciseAPI.lang}`, `${tests.code}`, "utf8", function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`testFile.${exerciseAPI.lang} was appended!`);
+        }
+    });
 }
 
-appendFile(`taskFile.${task.lang}`, `${task.code}`, "utf8", function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`taskFile.${task.lang} was appended!`);
-    }
-});
 
-appendFile(`testFile.${task.lang}`, `${test.code}`, "utf8", function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`testFile.${task.lang} was appended!`);
-    }
-});
+//example json converted to typescript
+const exerciseData: exerciseAPI = {
+    lang: "typescript",
+    code: "console.log('Hello, World!');",
+    exerciseID: "123456",
+    studentID: "789012"
+};
+
+const testData: tests = {
+    code: "console.log('Hello, World!');",
+};
+
+createDirectory(exerciseData, testData);
+

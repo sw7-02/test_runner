6b0fbd3f5e86ba61045dc8bc9fdb7067c07b692c
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2023-11-27 11:16:10 +0100

    some errors

--- src/test_runner.ts
+++ src/test_runner.ts
@@ -21,18 +21,6 @@ const exerciseTest = {
     "studentID": "67890",
     "testCases": [
         { "testCaseId": 1, "code": ` 
-#include <CUnit/CUnit.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void) {
-    CU_assert(addTwoNumbers(1,1) == 2);
-} 
-        `},
-        { "testCaseId": 2, "code": `
-<<<<<<< Updated upstream
-
         #include <CUnit/CUnit.h>
         #include <CUnit/Basic.h>
         #include <stdio.h>
@@ -70,26 +58,65 @@ int main(void) {
         
             // Print only if there are failures
             if (num_failures > 0) {
-                printf("\nTest failed\n Expected:");
+                printf("\\nTest failed\\n Expected:");
                 CU_basic_show_failures(CU_get_failure_list());
         
-                printf("\nActual: %d\n", addTwoNumbers(1, 2));
+                printf("\\nActual: %d\\n", addTwoNumbers(1, 2));
                 return 1;
             } else {
-                printf("Test passed\n");
+                printf("Test passed\\n");
                 return 0;
             }
         }
-=======
-#include <CUnit/CUnit.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(void) {
-    CU_assert(addTwoNumbers(1,2) == 2);
-}       
->>>>>>> Stashed changes
+        `},
+        { "testCaseId": 2, "code": `
+        #include <CUnit/CUnit.h>
+        #include <CUnit/Basic.h>
+        #include <stdio.h>
+        #include <stdlib.h>
+        
+        void testAddTwoNumbers(void) {
+            CU_ASSERT(addTwoNumbers(1, 2) == 2);
+        }
+        
+        int main(void) {
+            // Initialize the CUnit test registry
+            if (CUE_SUCCESS != CU_initialize_registry()) {
+                return CU_get_error();
+            }
+        
+            // Add a suite to the registry
+            CU_pSuite suite = CU_add_suite("Suite_1", NULL, NULL);
+            if (suite == NULL) {
+                CU_cleanup_registry();
+                return CU_get_error();
+            }
+        
+            // Add the test function to the suite
+            if (CU_add_test(suite, "testAddTwoNumbers", testAddTwoNumbers) == NULL) {
+                CU_cleanup_registry();
+                return CU_get_error();
+            }
+        
+        
+            // Run the tests using the basic interface
+            CU_basic_set_mode(CU_BRM_SILENT);
+            CU_basic_run_suite(suite);
+            
+            int num_failures = CU_get_number_of_failures();
+        
+            // Print only if there are failures
+            if (num_failures > 0) {
+                printf("\\nTest failed\\n Expected:");
+                CU_basic_show_failures(CU_get_failure_list());
+        
+                printf("\\nActual: %d\\n", addTwoNumbers(1, 2));
+                return 1;
+            } else {
+                printf("Test passed\\n");
+                return 0;
+            }
+        }
         `}
     ]
 };
@@ -111,14 +138,6 @@ testRunnerRunner(parsedExerciseTest);
 parsedExerciseTest.testCases.forEach(testCase => 
         compileAndRun(parsedExerciseTest.language, testCase.code, testCase.testCaseId));    
 
-parsedExerciseTest.testCases.forEach(testCase => {
-    compileAndRun(parsedExerciseTest.language, testCase.code, testCase.testCaseId)    
-
-});
-<<<<<<< Updated upstream
-//put respsonse JSON, when JSON array.length == parsedExerciseTest.testCases.length send response
-=======
-
 // put respsonse JSON, when JSON array.length == parsedExerciseTest.testCases.length send response
 
 /*
@@ -130,4 +149,3 @@ try {
     console.error(`Error deleting directory: ${err}`);
 }
 */
->>>>>>> Stashed changes

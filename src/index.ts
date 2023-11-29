e48faef01cab99b69d7cc4c4d9d052caef1a0bc3
lugialukas <lugialukas@live.dk>
2023-11-29 13:19:54 +0100

    imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-//import express from "express";
-import express = require("express");
+import express from "express";
+//import express = require("express");//Lukas skal køre det sådan her
 import * as bodyParser from "body-parser";
 import helmet from "helmet";
-//import cors from "cors";
-import cors = require("cors");
+import cors from "cors";
+//import cors = require("cors");//Lukas skal køre det sådan her
 import { ExerciseTest } from "./lib";
 import { runCode } from "./test_runner"
 
